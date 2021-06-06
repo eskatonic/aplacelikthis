e38c9b84c7f4d6b876c91976b03f78e7d0c3bd19
Brian Mays <brianmays@me.com>
2021-06-06 15:39:47 -0700

    Basic Views work, fixed issue with CSS

--- aplacelikethis/models.py
+++ aplacelikethis/models.py
@@ -3,6 +3,17 @@ from django.db import models
 from django.utils import timezone
 # User model from Django authentication system. No need to create another User model!
 from django.contrib.auth.models import User
+# reverse allows you to build URLs by their name and pass optional parameters
+from django.urls import reverse
+
+# objects is the default model manager, but you can add additional ones. PublishedManager
+# filters only published posts.
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+
 
 class Post(models.Model):
     STATUS_CHOICES = (
@@ -10,15 +21,21 @@ class Post(models.Model):
         ('published', 'Published'),
     )
     title = models.CharField(max_length=250)
+    # unique_for_date just makes sure each slug is unique on a given date
     slug = models.SlugField(max_length=250, unique_for_date='publish')
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     # auto_now_add adds the date automatically.
     created = models.DateTimeField(auto_now_add=True)
     # auto_now updates the date when you save.
     updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    status = models.CharField(
+        max_length=10, choices=STATUS_CHOICES, default='draft')
+    # Need to add objects here to keep it as the default
+    objects = models.Manager()  # Default model manager
+    published = PublishedManager()  # Published model manager
 
     # Meta contains metadata, used here for sorting posts by published date in
     # descending order ('-publish')
@@ -27,3 +44,12 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    # Returns 'Canonical" (aka Preferred) URL
+    # Use get_absolute_url in templates to link to specific posts
+    def get_absolute_url(self):
+        return reverse('aplacelikethis:post_details',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
---
+++ aplacelikethis/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from django.urls.resolvers import URLPattern
+from . import views
+
+# Use app_name to organize URLs by application. Here we have only one at the moment.
+# Best practice is to have a separate urls.py for each application.
+app_name = 'aplacelikethis'
+
+urlpatterns = [
+    # post views
+    path('', views.post_list, name='post_list'),
+    # path converters:
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
+         views.post_details,
+         name='post_details'),
+]

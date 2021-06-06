59217f46b7139c5699b82a78768c66474e8d10b5
Brian Mays <brianmays@me.com>
2021-06-06 09:42:52 -0700

    First commit - django installation successful

---
+++ settings.sql
@@ -0,0 +1,4 @@
+-- settings.sql
+CREATE DATABASE aplacelikethis;
+CREATE USER aplacelikethisuser WITH PASSWORD 'aplacelikethis';
+GRANT ALL PRIVILEGES ON DATABASE aplacelikethis TO aplacelikethisuser;

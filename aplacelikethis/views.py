90ae02a135c762e0969246b1fefd4bc074589043
Brian Mays <brianmays@me.com>
2021-06-06 19:29:49 -0700

    Fixed error in Send Email function

--- aplacelikethis/views.py
+++ aplacelikethis/views.py
@@ -5,6 +5,10 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # Adding class-based views:
 from django.views.generic import ListView
+# Adding to allow email:
+from .forms import EmailPostForm
+from django.core.mail import send_mail
+
 from .models import Post
 
 class PostListView(ListView):
@@ -39,3 +43,26 @@ def post_details(request, year, month, day, post):
     return render(request,
                   'aplacelikethis/post/details.html',
                   {'post': post})
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+
+    if (request.method == 'POST'):
+        form = EmailPostForm(request.POST)
+        if (form.is_valid()):
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url()
+            )
+            subject = f"{cd['name']} recommends you read " f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'admin@aplacelikethis.com',
+            [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'aplacelikethis/post/share.html',
+    {'post': post,
+    'form': form,
+    'sent': sent})
